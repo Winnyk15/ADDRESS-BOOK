2584f9c1315022bfac84876a692906d660054aa7
Winnyk15 <kahendahwinnie@gmail.com>
2020-07-30 15:47:26 +0300

    add changes

--- js/index.js
+++ js/index.js
@@ -67,11 +67,13 @@ $(document).ready(function() {
       });
     });
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-county").val("");
+    function resetFields() {
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+      $("input.new-street").val("");
+      $("input.new-city").val("");
+      $("input.new-county").val("");
+  }
 
   });
 });
